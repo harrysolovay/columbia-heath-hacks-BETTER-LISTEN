f6dc8c0e1f72ec16a8c9f244a136a15150d5358d
Chris Battista <cbattista19@gmail.com>
2018-04-15 09:28:59 -0400

    acuity testing implemented

--- src/index.js
+++ src/index.js
@@ -1,21 +1,31 @@
 
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import ReactDOM from 'react-dom'
 import WebFont from 'webfontloader'
 import {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts'
 import {SimpleLineChart} from './graph.js'
+import Tone from 'tone'
 
 import 'reset-css'
 import './style.css'
 
+var START_VOL = -20
+var START_TONE = 250
+var LAST_TONE = START_TONE * 4
+
 class App extends Component {
+
 
   state = {
     heading : 'Ready to Start?',
-    body : 'This test will perform a basic diagnostic test of your hearing and generate your unique hearing profile!',
+    body : 'This test will perform a basic diagnostic test of your hearing and generate your unique hearing profile.',
     count : 10,
     availableStateChange : 'PAUSE',
-    modalVisible : false
+    modalVisible : false,
+    introVisible : true,
+    testButtonVisible : false,
+    tone : START_TONE,
+    vol : START_VOL
   }
 
   constructor(props) {
@@ -26,6 +36,88 @@ class App extends Component {
         families : [ 'Raleway', 'Gamja Flower', 'Do Hyeon' ]
       }
     })
+
+    this.synth = new Tone.Synth().toMaster();
+    //this.volKnob = new Tone.Volume(this.state.vol);
+    //this.synth.chain(this.volKnob, Tone.Master);
+
+    this.volCallback = null;
+
+    this.acuity = [];
+  }
+
+  testTone() {
+    this.synth.volume.value = this.state.vol;
+    this.synth.triggerAttackRelease(this.state.tone, "4n");
+    this.volCallback = setTimeout(
+      this.advanceVol.bind(this), 4000
+    )
+  }
+
+  startTest() {
+    this.setState(
+      Object.assign(
+        {},
+        this.state,
+        {introVisible : false,
+        testButtonVisible : true,
+        heading : 'Testing',
+        body : ''}
+      )
+    )
+    this.testTone()
+  }
+
+  advanceTone() {
+    this.acuity.push({
+      freq : this.state.freq,
+      vol : this.state.vol
+    });
+    if (this.state.tone < LAST_TONE)
+    {
+      this.setState(
+        Object.assign(
+          {},
+          this.state,
+          {tone : this.state.tone * 2,
+          vol : START_VOL}
+        )
+      )
+
+      clearTimeout(this.volCallback);
+      setTimeout(
+        this.testTone.bind(this), Math.random()*1000+1000
+      )
+    }
+    else {
+      clearTimeout(this.volCallback);
+      this.showGraph()
+    }
+  }
+
+  advanceVol() {
+    this.setState(
+      Object.assign(
+        {},
+        this.state,
+        {vol : this.state.vol + 10}
+      )
+    )
+    setTimeout(
+      this.testTone.bind(this), Math.random()*1000+1000
+       )
+  }
+
+  showGraph() {
+    this.setState(
+      Object.assign(
+        {},
+        this.state,
+        {modalVisible : true,
+          introVisible : false,
+        testButtonVisible : false}
+      )
+    )
   }
 
   render() {
@@ -49,21 +141,32 @@ class App extends Component {
           </div>
           <div className='main'>
             <div className='center'>
-              <div className='heading'>{ this.state.heading }</div>
-              <div className='body'>{ this.state.body }</div>
-              <div className='action bad'></div>
-              <div className='action good'>some action</div>
+            <div className='heading'>{ this.state.heading }</div>
+            <div className='body'>{ this.state.body }</div>
+            {
+              this.state.introVisible && <React.Fragment>
+                <div className='action good' onClick={this.startTest.bind(this)}>Start!</div>
+              </React.Fragment>
+            }
+
+            {
+              this.state.testButtonVisible &&
+              <div className='action good' onClick={this.advanceTone.bind(this)}>Click every time you hear a tone.</div>
+            }
+
             </div>
           </div>
-          <div className='footer'>
-            <div>{ this.state.count }</div>
-            <div>{ this.state.availableStateChange }</div>
-          </div>
+          {/*
+            <div className='footer'>
+             <div>{ this.state.count }</div>
+             <div>{ this.state.availableStateChange }</div>
+           </div>
+           */}
         </div>
         <div id="modal" className={ this.state.modalVisible ? 'visibleModal' : 'hiddenModal' }>
           {
             this.state.modalVisible
-              ? <SimpleLineChart />
+              ? <SimpleLineChart data={this.acuity} />
               : null
           }
         </div>
